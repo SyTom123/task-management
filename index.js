22c84642d5821152dd5e754c0b03f4689c25a1e0
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-15 15:16:11 +0700

    Cài đặt cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const database = require("./config/database");
 const mongoose = require('mongoose');
 const routesApiVer1 = require("./api/v1/routes/index.route");
-
+const cors = require('cors');
 const app = express();
 
 
@@ -12,7 +12,9 @@ const app = express();
 database.connect();
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(cors()); // dùng để chia sẻ tài nguyên chéo nhau
 
 // Router Version 1
 routesApiVer1(app);
