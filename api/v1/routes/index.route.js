0158388f2917a9c4ed9ec63d339f8aa1f2429417
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-15 09:50:25 +0700

    6.2 + 6.3

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,4 +1,5 @@
 const taskRoutes = require("./task.route");
+
 module.exports = (app) => {
     const version = "/api/v1"
     app.use(version + "/tasks", taskRoutes);
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const Task = require("../../../models/task.model");
+const controller = require("../../v1/controller/task.controller");
 const router = express.Router();
-// const controller = require("../../");
 
-router.get("/", async (req , res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    res.json(tasks);
-});
+router.get("/", controller.index);
 
-router.get("/detail/:id", async (req , res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-        res.json(task);
-    } catch (error) {
-        res.json("Không tìm thấy")
-    }
-});
+router.get("/detail/:id", controller.detail);
+
 module.exports = router;
