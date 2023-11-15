c4f78b3662ed517db3caf83701b4311434015efe
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-15 14:17:37 +0700

    Đổi trạng thái một công việc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require("dotenv").config();
+const bodyParser = require("body-parser");
 const database = require("./config/database");
 const mongoose = require('mongoose');
 const routesApiVer1 = require("./api/v1/routes/index.route");
@@ -9,6 +10,9 @@ const app = express();
 
 //connect database
 database.connect();
+
+// parse application/json
+app.use(bodyParser.json())
 
 // Router Version 1
 routesApiVer1(app);
