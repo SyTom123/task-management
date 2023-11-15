eaa490a2da9e8e8ce3f7339df470bab76716db6d
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-15 14:59:47 +0700

    Xóa một và xóa nhiều

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -92,6 +92,18 @@ module.exports.changeMulti = async (req , res) => {
                     message: 'Cập nhật trạng thái thành công!'
                 });
                 break;
+            case "delete":
+                await Task.updateMany({
+                    _id: {$in: ids},
+                }, {
+                    deleted: true,
+                    deletedAt: new Date()
+                })
+                res.json({
+                    code: 200,
+                    message: 'Xóa thành công!'
+                });
+                break;
             default:
                 res.json({
                     code: 200,
@@ -135,6 +147,26 @@ module.exports.edit = async (req, res) => {
         res.json({
             code: 200,
             message: "Cập nhật thành công!",
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: 'Lỗi!'
+         });
+    }
+}
+// [DELETE]/api/v1/tasks/delete
+module.exports.delete = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await Task.updateOne({_id: id}, {
+            deleted: true,
+            deletedAt: new Date()
+        });
+
+        res.json({
+            code: 200,
+            message: "Xóa thành công!",
         })
     } catch (error) {
         res.json({
