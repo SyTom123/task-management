c1ece8df6ba875f16a286cd578959f186d527696
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-11-15 09:11:35 +0700

    Tạo router

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require("dotenv").config();
 const database = require("./config/database");
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const routesApiVer1 = require("./api/v1/routes/index.route");
 
 const app = express();
 
@@ -9,28 +10,10 @@ const app = express();
 //connect database
 database.connect();
 
-const Task = require('./models/task.model');
+// Router Version 1
+routesApiVer1(app);
 
-app.get("/tasks", async (req , res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    res.json(tasks);
-});
-app.get("/tasks/detail/:id", async (req , res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-        res.json(task);
-    } catch (error) {
-        res.json("Không tìm thấy")
-    }
-});
 
-// 
 const port = process.env.PORT;
 app.listen(port, ()=> {
     console.log(`App is listening on port ${port}`);
